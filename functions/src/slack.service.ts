2816fce04ff5a4550125c5435771427718bdc1dc
driflys <driflys@gmail.com>
2022-06-28 23:55:41 +0530

    move

--- functions/src/slack.service.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-require("dotenv").config();
-
-const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL || "";
-
-export const sendMessage = async (message: string) => {
-  return await axios.post(SLACK_WEBHOOK_URL, { text: message });
-};
